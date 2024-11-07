d3d100b040b221ca3d225bb9144a41fcba388b6c
339749939 <339749939@2260I-L29-015>
2024-11-07 09:26:53 -0500

    Add project files.

---
+++ Project2/Game1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Project2
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Texture2D dinotexture;
+        Texture2D chudtexture;
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.ApplyChanges();
+
+            this.Window.Title = "James cage fight";
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            dinotexture = Content.Load<Texture2D>("dino");
+            chudtexture = Content.Load<Texture2D>("Dancing");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(dinotexture, new Vector2(10, 10),  Color.White);
+            _spriteBatch.Draw(chudtexture, new Vector2(300, 180)  * 2, Color.White);
+
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
